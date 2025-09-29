705cd71b742e15fe9c34c265654e01580c1b0ce2
Hamala Samson <hamalasamsonlubiri@gmail.com>
2025-09-29 18:26:21 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~960c4522ebad45832df957133e35cecd1a66607a", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO~fced7aa1f387a13a3f819b2c7d8d9b8d3195ebb0", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
